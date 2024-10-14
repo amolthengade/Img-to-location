1ed2cad83afbfea0090670ac46ae2265afd51bb0
Amol Thengade <78148936+amolthengade@users.noreply.github.com>
2024-10-14 14:11:33 +0530

    Made more changes

--- app/loop.py
+++ app/loop.py
@@ -1,45 +1,10 @@
-# Example 1: Nested Loop for a 2D List (Matrix)
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
+import easyocr
 
-print("Example 1:")
-for row in matrix:
-    for element in row:
-        print(element, end=" ")
-    print()
-
-# Example 2: Nested Loop for Multiplication Table
-print("\nExample 2:")
-for i in range(1, 6):
-    for j in range(1, 11):
-        print(f"{i} x {j} = {i*j}")
-
-from PIL import Image
-import pytesseract
-
-def extract_text_from_image(image_path):
-    try:
-        # Open the image using Pillow
-        img = Image.open(image_path)
-
-        # Use pytesseract to extract text from the image
-        text = pytesseract.image_to_string(img)
-
-        return text
-
-    except Exception as e:
-        print(f"An error occurred: {e}")
-        return None
+def extract_text_with_easyocr(image_path):
+    reader = easyocr.Reader(['en'])
+    result = reader.readtext(image_path)
+    text = ' '.join([item[1] for item in result])
+    return text
 
 # Example usage
-image_path = 'c:/Users/HP/Desktop/pic.png'
-extracted_text = extract_text_from_image(image_path)
-
-if extracted_text:
-    print("Text extracted from the image:")
-    print(extracted_text)
-else:
-    print("Failed to extract text from the image.")
+print(extract_text_with_easyocr('C://Users/HP/Desktop/pic.png'))
--- app/models.py
+++ app/models.py
@@ -1,9 +1,1 @@
 from django.db import models
-class Student(models.Model):
-    name = models.CharField(max_length=50)
-    roll = models.IntegerField()
-    city = models.CharField(max_length=50)
-    email = models.EmailField(max_length=50)
-
-    def __str__(self):
-        return self.name
